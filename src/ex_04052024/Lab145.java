cc1084df33d51c6b1eff02c7b8fdf8cd1900b1ac
Meghabr50 <Meghanabrid50@gmail.com>
2024-05-21 22:04:20 +0530

    4 May 2024

--- src/ex_04052024/Lab145.java
+++ src/ex_04052024/Lab145.java
@@ -3,8 +3,6 @@ package ex_04052024;
 public class Lab145 {
     public static void main(String[] args) {
         String name = "Meghana";
-        for (int i = 0; i < ; i++) {
-            
-        }
     }
+
 }
